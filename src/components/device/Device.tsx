a56f9248431520860854b1de6fe8424a8795c289
Ben Shuyi Chen <ben.chen@intercom.io>
2020-06-15 19:02:50 -0700

    Add visor and logic for interacting w/ device

--- src/components/device/Device.tsx
+++ src/components/device/Device.tsx
@@ -1,14 +1,18 @@
-import React from "react";
-import classNames from 'classnames';
 import { makeStyles } from "@material-ui/core/styles";
-import deviceTarget from "../../images/device/Target.svg";
+import classNames from "classnames";
+import React from "react";
 import deviceCover from "../../images/device/Cover.svg";
 import deviceDial from "../../images/device/Dial.svg";
+import deviceTarget from "../../images/device/Target.svg";
+import deviceVisor from "../../images/device/Visor.svg";
+import { ButtonGroup, Button } from "@material-ui/core";
 
 const useStyles = makeStyles({
   root: {
     width: "100%",
     height: "100%",
+    display: "flex",
+    flexDirection: "column",
   },
   deviceContainer: {
     width: "100%",
@@ -16,26 +20,44 @@ const useStyles = makeStyles({
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
+    overflow: "hidden",
   },
   deviceImg: {
     position: "absolute",
-    width: "90%",
+    width: "70vmin",
     height: "auto",
-    maxWidth: "600px",
+    objectFit: "scale-down",
   },
   deviceScore: {
-    transition: "transform 1.5s ease-out",
+    transition: "transform 1.5s ease",
   },
-  deviceCover: { },
-  deviceDial: { },
+  deviceCover: {},
+  deviceDial: {
+    transition: "transform .6s ease",
+  },
+  deviceVisor: {
+    transition: "transform 2s ease",
+  },
+  buttonContainer: {
+    alignSelf: "center",
+    marginBottom: "20px",
+  },
 });
 
 export default function Dial() {
   const classes = useStyles();
-  const [rotationValue, setRotationValue] = React.useState(0);
+  const [scoreRotationValue, setScoreRotationValue] = React.useState(0);
+  const [dialRotationValue, setDialRotationValue] = React.useState(0);
+  const [visorRotationValue, setVisorRotationValue] = React.useState(0);
 
-  const randomRotationValue = () => {
-    setRotationValue(Math.random() * 360 + 180 + rotationValue);
+  const randomizeScore = () => {
+    setScoreRotationValue(Math.random() * 360 + 180 + scoreRotationValue);
+  };
+  const randomizeGuess = () => {
+    setDialRotationValue(Math.random() * 160 - 80);
+  };
+  const revealScore = () => {
+    visorRotationValue ? setVisorRotationValue(0) : setVisorRotationValue(170);
   };
 
   return (
@@ -44,10 +66,18 @@ export default function Dial() {
         <img
           className={classNames(classes.deviceImg, classes.deviceScore)}
           style={{
-            transform: `rotate(${rotationValue}deg)`,
+            transform: `rotate(${scoreRotationValue}deg)`,
           }}
           src={deviceTarget}
           alt="Device Score"
+        />
+        <img
+          className={classNames(classes.deviceImg, classes.deviceVisor)}
+          style={{
+            transform: `rotate(${visorRotationValue}deg)`,
+          }}
+          src={deviceVisor}
+          alt="Device Visor"
         />
         <img
           className={classNames(classes.deviceImg, classes.deviceCover)}
@@ -57,12 +87,24 @@ export default function Dial() {
         />
         <img
           className={classNames(classes.deviceImg, classes.deviceDial)}
-          style={{}}
+          style={{
+            transform: `rotate(${dialRotationValue}deg)`,
+          }}
           src={deviceDial}
           alt="Device Dial"
-          onClick={randomRotationValue}
+          onClick={randomizeScore}
         />
       </div>
+      <ButtonGroup
+        className={classes.buttonContainer}
+        variant="contained"
+        color="primary"
+        aria-label="contained primary button group"
+      >
+        <Button onClick={randomizeScore}>Reset</Button>
+        <Button onClick={randomizeGuess}>Guess</Button>
+        <Button onClick={revealScore}>Reveal</Button>
+      </ButtonGroup>
     </div>
   );
 }
