70ebdaca5fe75d48ff403cee15bd7ecfd7636b3e
Ben Shuyi Chen <ben.chen@intercom.io>
2020-06-18 01:21:47 -0700

    Add server w/ socket.io

--- client/src/components/app/App.tsx
+++ client/src/components/app/App.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 import Device from '../device/Device';
+import './App.css';
+import socketIOClient from 'socket.io-client';
+const SERVER_PORT = ":9000";
 
 function App() {
+  const [playerCount, setPlayerCount] = useState(0);
+
+  useEffect(() => {
+    const socket = socketIOClient(SERVER_PORT);
+    socket.on("new connection", (data: number) => {
+      setPlayerCount(data);
+    });
+    return function cleanup() {
+      socket.disconnect();
+    }
+  }, []);
+
   return (
     <div className="App">
       <Device/>
+      <div>
+        Connected: {playerCount}
+      </div>
     </div>
   );
 }
---
+++ server/server.js
@@ -0,0 +1,21 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(9000, () => {
+  console.log('listening on *:9000');
+});
+
+io.on('connection', (socket) => {
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+    io.emit('new connection', io.engine.clientsCount);
+  });
+
+  console.log('a user connected');
+  io.emit('new connection', io.engine.clientsCount);
+});
