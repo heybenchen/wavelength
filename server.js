7642e5be8596901f770cf4923ac8291d922bc8d5
Ben Shuyi Chen <ben.chen@intercom.io>
2020-06-18 17:41:21 -0700

    Update dependencies

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
