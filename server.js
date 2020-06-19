808f7d6df3acef7196afbf305b0b18dc101dc20f
Ben Shuyi Chen <ben.chen@intercom.io>
2020-06-18 21:03:51 -0700

    Serve production

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
