0f5a4ac44a9343397716384de04b57f3acc743d1
Ben Shuyi Chen <benshuyichen@gmail.com>
2020-06-27 00:54:23 -0700

    Remove unnecessary import

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
-const path = require("path");
 const io = require("socket.io")(http);
 const wordList = require("./words");
 
